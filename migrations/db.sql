2e0d8d7d43ec1746113ac6bc0e8efaa934a71af6
DouglasVolcato <douglasvolcato@gmail.com>
2025-11-01 18:34:48 -0300

    docs: add db diagram

---
+++ migrations/db.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+create table users (
+    id varchar(255) primary key,
+    name varchar(255),
+    email varchar(255),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+CREATE TRIGGER update_timestamp
+BEFORE UPDATE
+ON users
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+create table decks (
+    id varchar(255) primary key,
+    name varchar(255),
+    user_id varchar(255) references users(id),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+CREATE TRIGGER update_timestamp
+BEFORE UPDATE
+ON decks
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+create table flashcards (
+    id varchar(255) primary key,
+    question varchar(255),
+    answer varchar(255),
+    user_id varchar(255) references users(id),
+    deck_id varchar(255) references decks(id),
+    status varchar(255) default 'new' check (status in ('new', 'learning', 'mastered')),
+    review_count integer,
+    last_review_date timestamp,
+    next_review_date timestamp,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+CREATE TRIGGER update_timestamp
+BEFORE UPDATE
+ON flashcards
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
